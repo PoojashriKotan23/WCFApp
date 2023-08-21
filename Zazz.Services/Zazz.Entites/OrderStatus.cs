b1ec4f08809ad2944ebba0a2e581f3160beb691e
Poojashri <poojashri@abylle.com>
2023-08-21 18:33:22 +0530

    Entites Added

---
+++ Zazz.Services/Zazz.Entites/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zazz.Entites
+{
+ //Implicity data contract
+    internal class OrderStatus {
+       
+        public int Id { get; set; }  
+     
+        public string Name { get; set; }
+    }
+}
---
+++ Zazz.Services/Zazz.Services/IZass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zazz.Services 
+{
+    [ServiceContract]
+    public interface IZass 
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+        [OperationContract]
+        List<Customer> GetCustomer();
+        [OperationContract]
+        void SubmitOrder(Order order);
+
+    }
+}
